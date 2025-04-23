dc94b9e629313de95de2fe360acefe6cc768ee31
Rory Eddleston <rory_94@live.co.uk>
2025-04-23 20:24:20 +0100

    force scroll to top on each page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -8,9 +9,10 @@ import Contact from './pages/Contact';
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
+
       <div className="flex flex-col min-h-screen">
         <Navbar />
-
         <main className="flex-grow md:pl-20">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -18,7 +20,6 @@ const App = () => {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
-
         <Footer />
       </div>
     </Router>
