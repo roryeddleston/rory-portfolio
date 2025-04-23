246d6881b46de5f35ab5becaca85dcc9edfee90a
Rory Eddleston <rory_94@live.co.uk>
2025-04-23 09:06:55 +0100

    adjusted main app to account for sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer'; // ✅ Import the Footer
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
@@ -11,7 +11,7 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Navbar />
 
-        <main className="flex-grow">
+        <main className="flex-grow md:pl-20">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
