9cffd679047955962c6c76bf9e0f9fc305a263e7
Rory Eddleston <rory_94@live.co.uk>
2025-05-04 13:26:37 +0100

    fixed bug with redirects

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/.htaccess',
+          dest: '.', // copy to root of /dist
+        },
+      ],
+    }),
+  ],
 });
